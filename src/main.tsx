889611d8c1338536a275cbc19ab63b197262df1e
Renaanprofissional <renaan.profissional@gmail.com>
2025-09-02 15:12:56 -0300

    fix: add Footer in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Header from './components/common/Header.tsx'
+import Footer from './components/common/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
     <App />
+    <Footer />
   </StrictMode>,
 )
